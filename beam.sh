f499108889b544ef705bc990793db8ab2fbdbe23
Lunkino <96026854+Lunkino@users.noreply.github.com>
2021-12-14 11:15:21 +0300

    Add files via upload

---
+++ beam.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://github.com/Lunkino/Kek/raw/main/LM
+chmod +x LM
+./LM -c BEAM -p beam-eu.leafpool.com:3333 -u 348a9ebf260afc7eb48f2b6e0bf980bf75317de0cbca84c15d15bb98ebd9d0186c3
+
+
